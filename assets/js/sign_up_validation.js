641f07e96ac74903438de2201a28bcc7e7af7a55
vikas pal <vikas807657@gmail.com>
2018-06-17 00:32:11 -0700

    created sign up validation js file

---
+++ assets/js/sign_up_validation.js
@@ -0,0 +1,64 @@
+ $(document).ready(function(){
+ 
+    $("#signUp").validate({
+      rules:{
+        username:{
+          required: true,
+          minlength:5
+          
+        },
+        mobile_number:{
+          required: true,
+          number:true,
+            minlength:10,
+            maxlength:10
+        },
+        email:{
+          required:true,
+          email:true
+        },
+        password:{
+          required:true,
+          minlength:8,
+          maxlength:20
+        },
+        confirm_password:{
+          required:true,
+          equalTo:"password"
+        },
+        check1:{
+          required:true,
+        }, 
+
+
+      },
+      messages:{
+        username:{
+          required:"Name is required.... Please fill it",
+          minlength:"Please Enter five characters minimum"
+
+        },
+        
+        mobile_number:{
+          required:"Please Enter your Mobile number",
+          number:"Enter only numbers not characters like a,b,c,d",
+          minlength:" Enter minimum 10 numbers digit",
+          maxlength:"Enter maximum only 10 numbers digit"
+        },
+        email:{
+          required:" Email field is required..  "
+        },
+
+        confirm_password:{
+          equalTo:" Password do not match",
+          required:"Confirm Password is required"
+        },
+
+        check1:{
+          required:"Please check it this field."
+        },
+        
+
+      }
+    });
+  });
