ea35db8879b568a5ac84a022e2959b207e32b22d
vikas <vikaspal807657@gmail.com>
2018-07-14 02:37:03 -0700

    getDataFromEmail function added

--- guestHouse.php
+++ guestHouse.php
@@ -73,23 +73,20 @@ class GuestHouse
 	}
 
 
-	function saveIntoCateringTable(){
-		$sql = "INSERT INTO `guest_house`(`id`, `name`, `address`, `area`, `pincode`, `city`, `state`,`info`, `email`) VALUES (null, :name, :address, :area, :pincode, :city, :state, :info, :email)";
-
-	function getUserByEmail(){
-		$stmt=$this->conn->prepare("SELECT * FROM guest_house where email = :email");
-		echo $this->email;
-		$stmt->bindParam(':email',$this->email);
-		try {
-			if($stmt->execute()){
-				$user = $stmt->fetch(PDO::FETCH_ASSOC);
+		function getUserByEmail(){
+			$stmt=$this->conn->prepare('SELECT * FROM guest_house WHERE email = :email');
+			$stmt->bindParam(':email',$this->email);
+			try {
+				if($stmt->execute()){
+					$user = $stmt->fetch(PDO::FETCH_ASSOC);
+					}
 				}
+			catch(Exception $e) {
+					echo $e->getMessage();
+				}
+				return $user;
 			}
-		catch(Exception $e) {
-				echo $e->getMessage();
-			}
-			return $user;
-		}
+
 
 		function saveIntoCateringTable(){
 		$sql = "INSERT INTO `catering`(`id`, `name`, `address`, `area`, `pincode`, `city`, `state`, `info`, `email`) VALUES (null, :name, :address, :area, :pincode, :city, :state, :info, :email)";
