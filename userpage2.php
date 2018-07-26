e0d78b26a1dbfee8ae1f561a754d4bbdfa148887
vikas <vikaspal807657@gmail.com>
2018-07-26 03:36:42 -0700

    create method and model for update the information

--- userpage2.php
+++ userpage2.php
@@ -132,13 +132,149 @@
 
     <br>
     <br>
+
+    <!-- Trigger the modal with a button -->
+      
+      <!-- Modal -->
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+          <!-- Modal content-->
+          <div class="modal-content">
+            <form method="post" id="guest_update_form">
+                  <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      <h4 class="modal-title">Update Information Form</h4>
+                  </div>
+
+                  <div class="modal-body">
+                      <div class="col-md-6 col-md-offset-3">
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-leble="Close">x</button>
+                            <div id="result"></div>
+                      </div>
+                </div>
+                     <div class="form-group">
+                            <label>Guest House Name</label>
+                            <input type="text" name="gHouse_name" id="gHouse_name" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                            <label>Address</label>
+                            <input type="text" name="address" id="address" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                            <label>Pincode</label>
+                            <input type="number" name="pincode" id="pincode" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                            <label>City Or Distric</label>
+                            <input type="text" name="city" id="city" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                            <label>Website</label>
+                            <input type="text" name="url" id="url" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                            <label>Facility & Servicese</label>
+                            <input type="text" name="info" id="info" class="form-control" />
+                      </div>
+                  </div>
+
+                  <div class="modal-footer">
+                     <input type="submit" name="submit" class="btn btn-info pull-right" value="Update" />
+                        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                  </div>
+              
+            </form>
+            
+          </div>
+
+        </div>
+      </div>
+
+
     <footer class="container-fluid text-center">
-  <p>&copy;All Copy right www.EventsHall.com 2018-2019</p>
-  <div class="col-lg-12">
-    
+        <p>&copy;All Copy right www.EventsHall.com 2018-2019</p>
+        <div class="col-lg-12">
+          
         <a href="logout.php"><button style=" " class="btn btn-danger btn-lg glyphicon glyphicon-off"> SignOut</button></a>
-        <a href="updatepage.php"><button style=" " class="btn btn-danger btn-lg glyphicon glyphicon-pencil"> Update</button></a>
+        <button style=" " class="btn btn-danger btn-lg glyphicon glyphicon-pencil edit"> Update</button>
   </footer>
 <script src="assets/js/userfinalpage.js" type="text/javascript"></script>
+<script >
+
+  $(document).ready(function(){
+
+          $(document).on('click', '.edit', function(){
+                alert('hii');
+                var image_id = 1;
+                $.ajax({
+                 url:"edit.php",
+                 method:"post",
+                 data:{image_id:image_id},
+                 dataType:"json",
+                 success:function(data)
+                 {
+                  $('#myModal').modal('show');
+                  $('#gHouse_name').val(data.gname);
+                  $('#address').val(data.address);
+                  $('#pincode').val(data.pincode);
+                  $('#city').val(data.city);
+                  $('#url').val(data.url);
+                  $('#info').val(data.info);
+                 }
+                });
+           }); 
+
+         $('#guest_update_form').on('submit', function(event){
+
+              event.preventDefault();
+              if($('#gHouse_name').val() == '')
+              {
+               alert("Enter Guest House Name");
+              }
+              else if($('#address').val() == ''){
+                alert("Enter Address");
+              }
+               else if($('#city').val() == ''){
+                alert("Enter City Name");
+              }
+               else if($('#info').val() == ''){
+                alert("Enter Full description");
+              }
+               else if($('#pincode').val() == ''){
+                alert("Enter 6 digit pincode");
+              }
+              else
+              {
+              var form_data =$('#guest_update_form').serialize();
+              console.log(form_data)
+               $.ajax({
+                url:"action.php",
+                method:"POST",
+                data:form_data + '&action=update_gHouse'
+
+                }).done(function(result)
+                  {
+                   var data = JSON.parse(result); 
+                     $('.alert').show();
+                    if(data.status == 0){
+                      $('#result').html(data.msg);
+
+                    }else{
+                       alert('Information updated');
+                      document.location = 'userpage2.php';
+                    }
+            
+
+                });
+              };      
+
+    });
+
+        });
+  
+
+</script>
 </body>
 </html>
